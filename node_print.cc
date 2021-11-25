11feee47ed56b45660baabb89e3e667ae75db550
yrrho2 <yrrho2@naver.com>
2021-11-25 15:20:19 +0900

    nothing

--- node_print.cc
+++ node_print.cc
@@ -20,22 +20,17 @@
 #define BD "\u25cf"	//Black Dot
 #define WD "\u25cb"	//White Dot
 
-
-
 const void Node_Print_table::Print() const {
   int x = this->x_;
   int y = this->y_;
-  std::string line1_  = "";
+  std::string line1_  = "  ";
   std::string line2_ =  "   ";
 
-  //첫번째 초기출력 : 숫자들
   for(int i=0; i<x; i++) {
       line1_ += "   ";
-      //그냥 i로 정수 박으니까 매크로 나옴 ㄷㄷ
       line1_ += std::to_string(i);
   }
   std::cout << line1_ << std::endl;
-  //두번쨰 초기출력 : 테이블 최상단
   line2_ += LU;
   for(int i=0; i<x; i++) {
       line2_ += HL;
@@ -50,19 +45,18 @@ const void Node_Print_table::Print() const {
   std::cout << line2_ << std::endl;
 
 
-  //내용 출력
   for(int i=0; i<y; i++){
     line1_ = " " + std::to_string(i) + " " + VL;
     for(int j=0; j<x; j++){
       line1_ += SE;
       if (this->table_[i][j]->GetColor() == -1) {
         line1_ += SE;
+        line1_ += SE;
       } else if (this->table_[i][j]->GetColor() == 0) {
         line1_ += BD;
       } else if (this->table_[i][j]->GetColor() == 1) {
         line1_ += WD;
       }
-      line1_ += SE;
       line1_ += VL;
     }
     std::cout << line1_ << std::endl;
@@ -92,7 +86,6 @@ const void Node_Print_table::Print() const {
     std::cout << line2_ << std::endl;
   }
 }
-
 
 
 Node_Print_table* Node_Print_table::instance_ = NULL;
