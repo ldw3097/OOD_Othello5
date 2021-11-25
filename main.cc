b66afc6b168eb63fc0d36bec7e600768aefcb65c
yrrho2 <yrrho2@naver.com>
2021-11-25 15:52:09 +0900

    test

--- main.cc
+++ main.cc
@@ -6,8 +6,9 @@
 #include <string>
 
 int main(int argc, char** argv) {
-  int x=8; //x랑 y를 다르게 하면 바로 오류뜨네요
-  int y=8; //y가 작으면 돌아는 가는데 x가 작으면 터져버립니다,,, 수정 코맨트 환영
+	//hihiyoyoyo
+  int x=8; 
+  int y=8; 
 
   // int table
   int** temp_table;
